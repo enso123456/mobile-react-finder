39138c0b760ed62565ef80465e3abce99b43adc2
Romeo Enso <enso123456@gmail.com>
2018-04-17 22:16:07 +0800

    update server render

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static(path.resolve(__dirname, 'build')));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'public', "/index.html"));
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html');
 });
 
 app.listen(process.env.PORT || 3000);
