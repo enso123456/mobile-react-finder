f605c321124d117a317c26773914437fbeca3d6a
Romeo Enso <enso123456@gmail.com>
2018-04-17 22:26:44 +0800

    fix server js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, 'build')));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'build', 'index.html');
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
 app.listen(process.env.PORT || 3000);
